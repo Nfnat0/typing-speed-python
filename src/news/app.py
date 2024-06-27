a1dfa0196ae796607e6eabfc084bab5189b8c4ee
nfnat0 <mjtmwm@gmail.com>
2024-06-28 01:47:25 +0900

    複数ジャンルからの取得に変更 保存するパスを変更

--- src/news/app.py
+++ src/news/app.py
@@ -3,6 +3,11 @@ import requests
 import boto3
 import os
 from datetime import datetime, timezone
+import logging
+
+# Set up logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 ALLOWED_ORIGIN = os.environ['ALLOWED_ORIGIN']
 
@@ -13,56 +18,71 @@ def lambda_handler(event, context):
         "Access-Control-Allow-Headers": "Content-Type,X-CSRF-TOKEN",
         "Access-Control-Allow-Methods": "GET, OPTIONS",
     }
-    # Define the news API endpoint and parameters
+
     news_api_url = "https://newsapi.org/v2/top-headlines"
     ssm = boto3.client('ssm')
 
     parameter_name = os.environ['PARAMETER_NAME']
     response = ssm.get_parameter(Name=parameter_name, WithDecryption=True)
     api_key = response['Parameter']['Value']
-    params = {
-        'country': 'us',
-        'apiKey': api_key
-    }
 
-    # Fetch the latest news
-    response = requests.get(news_api_url, params=params)
-    news_data = response.json()
+    genres = ["business", "science", "technology"]
+    logger.info(f"Fetching news for genres: {genres}")
 
-    # Check if the request was successful
-    if response.status_code != 200:
-        return {
-            'statusCode': response.status_code,
-            'headers': headers,
-            'body': json.dumps(news_data)
+    s3_bucket_name = os.environ['S3_BUCKET_NAME']
+    s3 = boto3.client('s3')
+
+    for genre in genres:
+        params = {
+            'country': 'us',
+            'category': genre,
+            'apiKey': api_key
         }
 
-    # Retrieve the titles of the latest news
-    titles = [article['title'].split(' - ')[0]
-              for article in news_data['articles']]
+        logger.info(f"Fetching news for genre: {genre}")
+        response = requests.get(news_api_url, params=params)
+        news_data = response.json()
 
-    # Create the file content
-    file_content = "\n".join(titles)
+        if response.status_code != 200:
+            logger.error(f"Failed to fetch news for genre {genre}: {
+                         response.status_code} {news_data}")
+            continue
 
-    # Get the current time for the file name
-    execution_time = datetime.now(timezone.utc).strftime('%Y%m%d')
+        # Retrieve and filter the titles of the latest news
+        titles = [
+            article['title'].split(' - ')[0] for article in news_data['articles']
+            if '[Removed]' not in article['title']
+        ]
 
-    # Define the file name
-    file_name = f"{execution_time}.txt"
+        if not titles:
+            logger.info(f"No valid news titles found for genre {genre}")
+            continue
 
-    # Get the S3 bucket name from the environment variables
-    s3_bucket_name = os.environ['S3_BUCKET_NAME']
+        file_content = "\n".join(titles)
 
-    # Save the file to S3
-    s3 = boto3.client('s3')
-    s3.put_object(
-        Bucket=s3_bucket_name,
-        Key=file_name,
-        Body=file_content.encode('utf-8')
-    )
+        # Get the current time for the file name
+        execution_time = datetime.now(timezone.utc).strftime('%Y/%m/%d')
+
+        # Define the file name
+        file_name = f"{execution_time}/{genre}.txt"
+
+        # Save the file to S3
+        try:
+            logger.info(f"Saving news for genre {genre} to S3 bucket {
+                        s3_bucket_name} with file name {file_name}")
+            s3.put_object(
+                Bucket=s3_bucket_name,
+                Key=file_name,
+                Body=file_content.encode('utf-8')
+            )
+            logger.info(f"Successfully saved news for genre {
+                        genre} to {file_name}")
+        except Exception as e:
+            logger.error(f"Error saving news for genre {
+                         genre} to S3: {str(e)}")
 
     return {
         'statusCode': 200,
         'headers': headers,
-        'body': json.dumps('File saved to S3 successfully')
+        'body': json.dumps('News files saved to S3 successfully')
     }
